2b332bcbdb01ccd6760852143f0ac3b955b052e3
ThiagoLuzFerreira <tlrferreira83@gmail.com>
2022-12-30 08:18:12 -0300

    first commit

---
+++ src/test/java/br/com/springmongodb/SpringmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
