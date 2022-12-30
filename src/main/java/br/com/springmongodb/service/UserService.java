298d47574cf1283f8aef039533e9d4ed3bef99b4
ThiagoLuzFerreira <tlrferreira83@gmail.com>
2022-12-30 16:39:23 -0300

    mongodb connection, repository and service

---
+++ src/main/java/br/com/springmongodb/service/UserService.java
@@ -0,0 +1,19 @@
+package br.com.springmongodb.service;
+
+import br.com.springmongodb.domain.User;
+import br.com.springmongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+}
